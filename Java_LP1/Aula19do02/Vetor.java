a50db73f8cdd22318e009f8dc78ad881da9453fb
rafael <rafael.rmo.123456789@gmail.com>
2024-02-19 17:16:57 -0400

    Aula 19 de Fevereiro

--- Java_LP1/Aula19do02/Vetor.java
+++ Java_LP1/Aula19do02/Vetor.java
@@ -2,11 +2,20 @@ package Java_LP1.Aula19do02;
 
 public class Vetor {
     public static void main(String[] args) {
-        int v [] = new int[10];
-         for(int i =0;i <11;i++)
-        {
-            v[i]= i * 2;
-            System.out.println(v[i]);
-        }        
+      //  int v [] = new int[10];
+      //   for(int i =0;i <v.length;i++)
+      //  {
+      //      v[i]= i * 2;
+      //      System.out.println(v[i]);
+      //  }   
+      
+      int x [][] = new int [3][3];
+      for(int i = 0; i<3; i++){
+        System.out.print("\n");
+        for(int j =0; j<3; j++){
+            x[i][j] = i+j;
+            System.out.print(x[i][j]+"\t");
+        }
+      }
     }
 }
