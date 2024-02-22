20c87fb4168d397e98cb799b7d0cc73e7ea7a7c2
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2024-02-22 17:16:44 -0400

    Update TestaTeclado.java

--- Java_LP1/Aula22do02/TestaTeclado.java
+++ Java_LP1/Aula22do02/TestaTeclado.java
@@ -1,3 +1,4 @@
+
 public class TestaTeclado {
 
 	public static void main(String[] args) {
@@ -7,14 +8,18 @@ public class TestaTeclado {
 		v [i] = numero;
 		}
 		int maior = 0;
+		int menor = 100000000;
 		for (int j = 0; j < v.length; j++)
 		{
 			if (v[j]>maior) {
 				maior = v[j];
 			}
+			if (v[j]<menor) {
+				menor = v[j];
+			}
 		}
 		System.out.println("O maior numero digitado:"+maior);
-		
+		System.out.println("O menor numero digitado:"+menor);
 
 	}
 
