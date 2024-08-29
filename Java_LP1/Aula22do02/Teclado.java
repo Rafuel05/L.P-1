9dae9d781517c63aa9012166f30022db2db2427c
Rafael Melchior <118372879+Rafuel05@users.noreply.github.com>
2024-08-29 16:05:38 -0400

    Update Teclado.java

--- Java_LP1/Aula22do02/Teclado.java
+++ Java_LP1/Aula22do02/Teclado.java
@@ -1,20 +1,43 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Teclado {
 
 	public static int lerInteiro(String rotulo) {
 		System.out.println(rotulo);
-		Scanner scann = new Scanner (System.in);
-		return scann.nextInt();
+		try {
+			Scanner scann = new Scanner (System.in);
+			return scann.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("Digite apenas numeros inteiros");
+			System.out.println(rotulo);
+			Scanner scann = new Scanner (System.in);
+			return scann.nextInt();
+		}
 	}
 	public static double lerDouble (String rotulo) {
-		System.out.println(rotulo);
-		Scanner scann = new Scanner (System.in);
-		return scann.nextDouble();
+		try {
+			System.out.println(rotulo);
+			Scanner scann = new Scanner (System.in);
+			return scann.nextDouble();
+		} catch (InputMismatchException e) {
+			System.out.println("Digite apenas numeros com virgula (doubles), ou qualquer numero do conjunto dos reais");
+			System.out.println(rotulo);
+			Scanner scann = new Scanner (System.in);
+			return scann.nextDouble();
+		}
 	}
 	
 	public static String lerString(String rotulo) {
-		System.out.println(rotulo);
-		Scanner scann = new Scanner (System.in);
-		return scann.nextLine();
+		try {
+			System.out.println(rotulo);
+			Scanner scann = new Scanner (System.in);
+			return scann.nextLine();
+		} catch (InputMismatchException e) {
+			System.out.println("Digite apenas textos");
+			System.out.println(rotulo);
+			Scanner scann = new Scanner (System.in);
+			return scann.nextLine();
+		}
+		
 	}
 }
